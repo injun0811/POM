e47824742219f6bb6cd9fd2d1c6bfadb6a5bf5da
injun <jun2000005@naver.com>
2025-08-21 13:50:57 +0900

    전체적인 Update - 2025.03

---
+++ src/styled/Global.js
@@ -0,0 +1,54 @@
+import { createGlobalStyle } from "styled-components";
+import styled from "styled-components";
+
+const GolbalStyle = createGlobalStyle`
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+
+    body {
+        font-family: Arial, sans-serif;
+        font-size: 1rem;
+    }
+
+    ul {
+        list-style-type: none;
+    }
+
+    a {
+        text-decoration: none;
+        color: inherit;
+    }
+
+    :root {
+        --main-background: #eaddca;
+        --main-border: #daa06d;
+    }
+`;
+
+export const Background = styled.div`
+    width: 100%;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    background: #121212;
+    background: linear-gradient(135deg, #121212 25%, #1a1a1a 25%, #1a1a1a 50%, #121212 50%, #121212 75%, #1a1a1a 75%, #1a1a1a);
+    background-size: 40px 40px;
+
+    animation: move 4s linear infinite;
+
+    @keyframes move {
+        0% {
+            background-position: 0 0;
+        }
+        100% {
+            background-position: 40px 40px;
+        }
+    }
+`;
+
+export default GolbalStyle;
