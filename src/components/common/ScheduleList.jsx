1079d3141b89ae9f57c71ad07d48168a74f27107
injun <jun2000005@naver.com>
2025-08-29 18:28:07 +0900

    일정 등록 예시 데이터 표시

--- src/components/common/ScheduleList.jsx
+++ src/components/common/ScheduleList.jsx
@@ -4,11 +4,18 @@ import { ScheduleLi } from "../../styled/common/ScheduleList";
 const ScheduleList = () => {
     return (
         <ScheduleLi>
+            {/* https://uiverse.io/SouravBandyopadhyay/rude-tiger-29 */}
+            {/* https://uiverse.io/Samalander0/sweet-eel-98 */}
+
             {/* 하루 종일 일정 - 리스트 DIV 맨 위에 줄 길게 생성 처리 */}
 
             {/* 완료 - 일정 내용 글자에 가운데 줄 긋기 처리 */}
+            <p>styled-component 정리</p>
 
             {/* 휴일 - 날짜 표시에 빨간색으로 처리 (보통은 흰색, 검정색 처리) */}
+            <div>
+                <img src="" />
+            </div>
 
             {/* 장소 - 아이콘 처리 후 마우스 올리면 DIV 생성되어 주소와 지도API 표시 */}
 
--- src/styled/common/ScheduleList.js
+++ src/styled/common/ScheduleList.js
@@ -1,3 +1,14 @@
 import styled from "styled-components";
 
-export const ScheduleLi = styled.li``;
+export const ScheduleLi = styled.li`
+    width: 320px;
+    height: 80px;
+
+    p {
+        width: 100%;
+        height: 100%;
+        padding: 0 0 30px 0;
+        font-size: 20px;
+        align-content: center;
+    }
+`;
