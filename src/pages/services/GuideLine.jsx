1b3596c0929dbd3e19f771476cc4e53246ae6844
injun <jun2000005@naver.com>
2025-08-29 18:28:20 +0900

    supabase 연동

--- src/pages/services/GuideLine.jsx
+++ src/pages/services/GuideLine.jsx
@@ -1,12 +1,46 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import supabase from "../../services/supabaseClient";
 
 const GuideLine = () => {
-    return (
-        <div>
-            <h1>서비스 가이드</h1>
-            <h2>Guideline</h2>
-        </div>
-    );
+    const [tests, setTest] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const fetchTest = async () => {
+            const { data, error } = await supabase.from("testTable").select("*");
+            if (error) console.error("Error fetching items:", error);
+            else {
+                console.log("data:", data);
+                setTest(data || []);
+                setLoading(false);
+            }
+        };
+        fetchTest();
+    }, []);
+
+    if (loading)
+        return (
+            <>
+                <h1>서비스 가이드</h1>
+                <h2>Guideline</h2>
+                loading...
+            </>
+        );
+    else {
+        return (
+            <>
+                <h1>서비스 가이드</h1>
+                <h2>Guideline</h2>
+                <ul>
+                    {tests.map((test) => (
+                        <li key={test.seq_no}>
+                            {test.seq_no} / {test.text}
+                        </li>
+                    ))}
+                </ul>
+            </>
+        );
+    }
 };
 
 export default GuideLine;
