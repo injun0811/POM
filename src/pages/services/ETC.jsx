cae794daedc243cb708dca9dad9ae2b5a1eac408
injun <jun2000005@naver.com>
2025-10-20 14:18:41 +0900

    dayjs & DatePicker 라이브러리 사용 및 ECT에 기능 모음 처리

--- src/pages/services/ETC.jsx
+++ src/pages/services/ETC.jsx
@@ -1,19 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
+import DatePicker from "react-datepicker";
+import dayjs from "dayjs";
+import "react-datepicker/dist/react-datepicker.css";
 import { ETCDiv } from "../../styled/services/ETC";
 import IconButton from "../../components/common/ui/IconButton";
 
 const ETC = () => {
+    const [date, setDate] = useState(new Date());
     return (
         <ETCDiv>
             <h1>소규모 기능</h1>
             <h2>ETC</h2>
 
-            <IconButton func={"Download"}></IconButton>
-            <IconButton func={"Delete"}></IconButton>
-            <IconButton func={"Cancel"}></IconButton>
-            <IconButton func={"Update"}></IconButton>
-            <IconButton func={"Reset"}></IconButton>
-            <IconButton func={"Save"}></IconButton>
+            <section className="flex">
+                <div className="step one">
+                    <h3>아이콘 활용한 버튼 컴포너트</h3>
+                    <IconButton func={"Download"}></IconButton>
+                    <IconButton func={"Delete"}></IconButton>
+                    <IconButton func={"Cancel"}></IconButton>
+                    <IconButton func={"Update"}></IconButton>
+                    <IconButton func={"Reset"}></IconButton>
+                    <IconButton func={"Save"}></IconButton>
+                </div>
+
+                <div className="step">
+                    <h3>DatePicker 라이브러리</h3>
+                    <DatePicker seleted={date} onChange={(d) => setDate(d)} dateFormat="yyyy-MM-dd" />
+                    <p>선택한 날짜 : {dayjs(date).format("YYYY-MM-DD")}</p>
+                </div>
+            </section>
         </ETCDiv>
     );
 };
--- src/styled/services/ETC.js
+++ src/styled/services/ETC.js
@@ -1,3 +1,17 @@
 import styled from "styled-components";
 
-export const ETCDiv = styled.div``;
+export const ETCDiv = styled.section`
+    .flex {
+        display: flex;
+        gap: 20px;
+
+        .step {
+            padding: 15px;
+            border-radius: 20px;
+            border: 3px solid black;
+        }
+        .one {
+            justify-items: center;
+        }
+    }
+`;
